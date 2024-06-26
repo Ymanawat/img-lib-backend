5216ed13b0506ce914ad3a7804985f9db1dc0304
Yogendra Manawat <yogendramanawat@gmail.com>
2024-06-26 20:20:43 +0530

    login signup fix

--- user/views.py
+++ user/views.py
@@ -15,16 +15,16 @@ def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        user = User.objects.get(email=request.data['email'])
+        user = User.objects.get(username=request.data['username'])
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({'token': token.key, 'user': serializer.data})
-    return Response(serializer.errors, status=status.HTTP_200_OK)
+        return Response({'token': token.key})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(User, email=request.data['email'])
+    user = get_object_or_404(User, username=request.data['username'])
     if not user.check_password(request.data['password']):
         return Response("missing user", status=status.HTTP_404_NOT_FOUND)
     token, created = Token.objects.get_or_create(user=user)
